57588e4fd4d86cabe40c60ab2d395fed7c593325
shoyabsyed <sdshoyab46@gmail.com>
2024-10-03 12:57:39 +0530

    favicon ico update

--- src/components/footer/SubFooter.jsx
+++ src/components/footer/SubFooter.jsx
@@ -3,7 +3,7 @@ const SubFooter = () => {
     <div className="bg-slate-800 flex flex-col md:flex-row justify-between items-center text-white p-6">
       <div className="mb-4 md:mb-0">
         <p className="text-xs font-semibold text-center md:text-left">
-          &copy; 2024 Prime Hospital Guntur - Powered by Rishi
+          &copy; 2024 Prime Hospital Guntur - Powered by SH Technologies
         </p>
       </div>
       <div className="flex gap-4">
